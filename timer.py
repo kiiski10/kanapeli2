d24bc657acbd5495d206ee99a15a585acd810623
kiiski10 <mavakatti@gmail.com>
2021-08-20 16:58:33 +0300

    ..and the actual file for TimedActions

---
+++ timer.py
@@ -0,0 +1,13 @@
+import time
+
+class TimedAction:
+	def __init__(self, name, interval, action):
+		self.name = name
+		self.action = action
+		self.interval = interval
+		self.lastTick = time.time() * 1000
+
+	def activate(self):
+		if time.time() * 1000 - self.lastTick > self.interval:
+			self.lastTick = time.time() * 1000
+			self.action()
